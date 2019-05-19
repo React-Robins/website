5b8cfc720e208033a6166a33b21027a1523c46fc
Monica Powell <monica@aboutmonica.com>
2019-05-19 12:52:09 -0400

    update github link

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -12,7 +12,7 @@ import TwitterIcon from '@static/icons/twitter.svg';
 const SOCIAL = [
   {
     icon: GithubIcon,
-    link: 'https://github.com/M0nica/react-ladies-site',
+    link: 'https://github.com/M0nica/react-ladies',
   },
 
   {
