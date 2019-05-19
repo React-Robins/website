a27d17f68537b065d20e45bfa07091c84e689234
Monica Powell <monica@aboutmonica.com>
2019-05-19 14:10:12 -0400

    update SEO information

--- src/components/common/SEO.js
+++ src/components/common/SEO.js
@@ -2,29 +2,24 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 const SEO_DATA = {
-  description: 'An absurd gatsby starter',
+  description: 'A community for women and non-binary ReactJS engineers',
   title: 'React Ladies',
-  url: '',
+  url: 'www.reactladies.com',
   author: 'Monica Powell',
   keywords: ['gatsby', 'site'],
   twitter: {
     id: '@waterproofheart',
-    img: '',
-  },
-  facebook: {
-    id: '',
-    img: '',
+    img: 'src/images/team/React-icon.png',
   },
 };
 
 const SEO = () => {
   return (
     <Helmet>
-      <meta property="fb:app_id" content={SEO_DATA.facebook.id} />
       <meta property="og:title" content={SEO_DATA.title} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={SEO_DATA.url} />
-      <meta property="og:image" content={SEO_DATA.facebook.img} />
+      <meta property="og:image" content={SEO_DATA.twitter.img} />
       <meta property="og:description" content={SEO_DATA.description} />
 
       <meta name="twitter:card" content="summary_large_image" />
