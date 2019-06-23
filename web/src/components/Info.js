75037b3f7267d8faca71b75347a605a346b99ca1
Sara <hey@iamsaravieira.com>
2019-06-23 18:56:51 +0200

    add rsvp

--- web/src/components/Info.js
+++ web/src/components/Info.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import format from 'date-fns/format'
 
 import Flag from './icons/flag'
 import Calendar from './icons/calendar'
+import sanityClient from '@sanity/client'
 
 const Info = styled.div`
   display: flex;
@@ -11,7 +12,7 @@ const Info = styled.div`
   border: 3px solid ${props => props.theme.white};
   align-items: center;
   font-size: 16px;
-  margin-bottom: 40px;
+  margin-bottom: 10px;
   font-family: 'Roboto Mono';
   position: relative;
   justify-content: space-between;
@@ -31,19 +32,131 @@ const Info = styled.div`
   }
 `
 
-export default ({site}) => {
-  return (
-    <Info>
-      <Flag />
-      <span>
-        Location:{' '}
-        <a href='https://goo.gl/maps/VV6YUwPJaT79ESGG9' target='_blank'>
-          {site.location}
-        </a>
-      </span>
+const Button = styled.button`
+  height: 52px;
+  background: ${props => props.theme.white};
+  -webkit-appearance: none;
+  border: none;
+  width: 100%;
+  font-family: 'Roboto Mono';
+  font-weight: 600;
+  font-size: 18px;
+  margin-bottom: 50px;
 
-      <span>{format(site.date, ['HH:mm Do [of] MMMM '])}</span>
-      <Calendar />
-    </Info>
+  span {
+    text-decoration: underline;
+  }
+`
+
+const Form = styled.form`
+  background: ${props => props.theme.white};
+  -webkit-appearance: none;
+  border: none;
+  width: 100%;
+  font-family: 'Roboto Mono';
+  font-weight: 600;
+  font-size: 18px;
+  margin-bottom: 50px;
+  color: ${props => props.theme.darkPurple};
+  padding: 20px;
+  height: 52px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  input {
+    padding: 8px 12px;
+    border: 1px solid ${props => props.theme.lightGrey};
+    border-radius: 4px;
+  }
+
+  label {
+    text-decoration: underline;
+    margin-right: 10px;
+  }
+`
+
+const Submit = styled.button`
+  border: none;
+  padding: 2px 12px;
+  color: ${props => props.theme.white};
+  font-weight: 600;
+  background: ${props => props.theme.darkPurple};
+  transition: all 200ms ease;
+  cursor: pointer;
+  border-radius: 4px;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+//
+
+export default ({site}) => {
+  const [open, setOpen] = useState(false)
+  const [name, setName] = useState('')
+  const [gh, setGH] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+
+  const client = sanityClient({
+    projectId: 'atggkqis',
+    dataset: 'production',
+    token:
+      'skHtAQAA3FfHcKm6kRQmBNaE2Puzw0BqoKkBBJeXCrezlGioYwp3YywbFVRcRiXuptoCOT9CFAI1rBrVq5nqrGw6FdvYWmtitz69eIO5LynHtfgHh0yiM3dfVLaOY9HP98DAasbWwmebtL4ldtKRFjW1ZXx9MjFIDtIG2eTYznHOhIIzifm9', // or leave blank to be anonymous user
+    useCdn: true
+  })
+
+  const createUser = () => {
+    const doc = {
+      _type: 'attendee',
+      name: name,
+      ghLink: gh
+    }
+
+    client.create(doc).then(res => {
+      console.log(`Human was created, document ID is ${res._id}`)
+    })
+  }
+
+  return (
+    <>
+      <Info>
+        <Flag />
+        <span>
+          Location:{' '}
+          <a href='https://goo.gl/maps/VV6YUwPJaT79ESGG9' target='_blank'>
+            {site.location}
+          </a>
+        </span>
+
+        <span>{format(site.date, ['HH:mm Do [of] MMMM '])}</span>
+        <Calendar />
+      </Info>
+      {!open ? (
+        <Button onClick={() => setOpen(true)}>
+          {'>>>>'} {!submitted ? <span>RSVP NOW</span> : <span>YOU ARE AWESOME</span>} {'<<<<'}
+        </Button>
+      ) : (
+        <Form
+          onSubmit={e => {
+            e.preventDefault()
+            setOpen(false)
+            setSubmitted(true)
+          }}
+        >
+          <div>
+            <label htmlFor='name'>Name</label>
+            <input value={name} onChange={e => setName(e.target.value)} id='name' type='text' />
+          </div>
+          <div>
+            <label htmlFor='gh'>Github Username</label>
+            <input id='gh' value={gh} onChange={e => setGH(e.target.value)} type='text' />
+          </div>
+
+          <Submit onClick={createUser}>ADD HUMAN</Submit>
+        </Form>
+      )}
+    </>
   )
 }
