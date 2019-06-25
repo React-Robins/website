9b67dce1d3e6ebf442764c5b30b3a351c2ce266b
Sara <hey@iamsaravieira.com>
2019-06-25 13:31:08 +0200

    fix be

--- web/src/components/Figure.js
+++ web/src/components/Figure.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Img from 'gatsby-image'
 import { getFluidGatsbyImage } from 'gatsby-source-sanity'
-import clientConfig from '../../client-config'
+
+const config = {
+  projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'atggkqis',
+  dataset: process.env.GATSBY_SANITY_DATASET || 'production',
+  token: process.env.SANITY_READ_TOKEN
+}
 
 export default ({ node }) => {
-  const fluidProps = getFluidGatsbyImage(node.asset._id, { maxWidth: 675 }, ...clientConfig.sanity)
+  const fluidProps = getFluidGatsbyImage(node.asset._id, { maxWidth: 675 }, config)
   return <Img fluid={fluidProps} alt={node.alt} />
 }
