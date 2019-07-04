9a95c41de910da0ad60e954b118f8fd83f756cdb
Sara <hey@iamsaravieira.com>
2019-07-04 21:41:26 +0200

    add moire peeps

---
+++ web/src/pages/_organizers.js
@@ -0,0 +1,72 @@
+import { gayStripes, aceStripes, biStripes, fluidStripes } from '../helpers/useRainbow'
+
+export default {
+  main: [
+    {
+      name: 'Sara Vieira',
+      twitterLink: 'NikkitaFTW',
+      image: 'https://pbs.twimg.com/profile_images/1123259650126774272/KOE88EYU_400x400.jpg',
+      colors: gayStripes
+    },
+    {
+      name: 'Laura Gonzalez',
+      twitterLink: 'freezydorito',
+      image: 'https://pbs.twimg.com/profile_images/1092069028154216449/M-yDW4aY_400x400.jpg',
+      colors: aceStripes
+    },
+    {
+      name: 'Kate Beard',
+      twitterLink: 'SBinLondon',
+      image: 'https://pbs.twimg.com/profile_images/1094904470121734144/IzqaUOrJ_200x200.jpg',
+      colors: biStripes
+    },
+    {
+      name: 'Carolyn Stransky',
+      twitterLink: 'carolstran',
+      image: 'https://newdevs.org/images/coach_carolyn.png',
+      colors: biStripes
+    }
+  ],
+  amsterdam: [
+    {
+      name: 'Paul Verbeek-Mast',
+      twitterLink: 'paul_v_m',
+      image: 'https://pbs.twimg.com/profile_images/1002524944968880129/5TejN8df_200x200.jpg',
+      colors: fluidStripes
+    }
+  ],
+  barcelona: [
+    {
+      name: 'Jeremias Menichelli',
+      twitterLink: 'jeremenichelli',
+      image: 'https://pbs.twimg.com/profile_images/1104048118490980354/813En9fK_400x400.jpg',
+      colors: []
+    },
+    {
+      name: 'Daniel Ehrenberg',
+      twitterLink: 'littledan',
+      image: 'https://pbs.twimg.com/profile_images/1117445887335096320/4NHfCPH2_400x400.jpg',
+      colors: []
+    },
+    {
+      name: 'Ignacio Villanueva',
+      twitterLink: 'IgnaciodeNuevo',
+      image: 'https://pbs.twimg.com/profile_images/1050497062448586752/8F9sTc4s_400x400.jpg',
+      colors: []
+    },
+    {
+      name: 'Calabria',
+      twitterLink: 'Zurribulle',
+      image: 'https://pbs.twimg.com/profile_images/879004825496633344/Xwi4sQFe_400x400.jpg',
+      colors: []
+    }
+  ],
+  'San Francisco': [
+    {
+      name: 'Alejandra',
+      twitterLink: 'AlxSavage',
+      image: 'https://pbs.twimg.com/profile_images/1102602157990297601/FDgnWlcd_400x400.jpg',
+      colors: gayStripes
+    }
+  ]
+}
--- web/src/pages/organizers.js
+++ web/src/pages/organizers.js
@@ -3,50 +3,13 @@ import SEO from '../components/seo'
 import Layout from '../containers/layout'
 import Panel from '../components/Panel'
 import Speakers from '../components/Speakers'
-import { gayStripes, aceStripes, biStripes, fluidStripes } from '../helpers/useRainbow'
 
-const organizers = [
-  {
-    id: 1,
-    name: 'Sara Vieira',
-    twitterLink: 'NikkitaFTW',
-    image: 'https://pbs.twimg.com/profile_images/1123259650126774272/KOE88EYU_400x400.jpg',
-    location: 'Berlin, Germany 🇩🇪',
-    colors: gayStripes
-  },
-  {
-    id: 2,
-    name: 'Laura Gonzalez',
-    twitterLink: 'freezydorito',
-    image: 'https://pbs.twimg.com/profile_images/1092069028154216449/M-yDW4aY_400x400.jpg',
-    location: 'London, UK 🇬🇧',
-    colors: aceStripes
-  },
-  {
-    id: 3,
-    name: 'Kate Beard',
-    twitterLink: 'SBinLondon',
-    image: 'https://pbs.twimg.com/profile_images/1094904470121734144/IzqaUOrJ_200x200.jpg',
-    location: 'London, UK 🇬🇧',
-    colors: biStripes
-  },
-  {
-    id: 4,
-    name: 'Paul Verbeek-Mast',
-    twitterLink: 'paul_v_m',
-    image: 'https://pbs.twimg.com/profile_images/1002524944968880129/5TejN8df_200x200.jpg',
-    location: 'Amsterdam, Netherlands 🇳🇱',
-    colors: fluidStripes
-  },
-  {
-    id: 5,
-    name: 'Carolyn Stransky',
-    twitterLink: 'carolstran',
-    image: 'https://newdevs.org/images/coach_carolyn.png',
-    location: 'Berlin, Germany 🇩🇪',
-    colors: biStripes
-  }
-]
+import organizers from './_organizers'
+
+const capitalize = s => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
 
 const CodeOfConduct = ({ location }) => {
   const { site } = location.state || {
@@ -65,6 +28,7 @@ const CodeOfConduct = ({ location }) => {
             Nothing can be organized by only one person and there is a team helping QueerJS be a
             safe space all over the world.
           </p>
+          <p>The cities indicate what events they helped organize</p>
           <p
             css={`
               margin-bottom: 40px;
@@ -72,7 +36,20 @@ const CodeOfConduct = ({ location }) => {
           >
             Thank you.
           </p>
-          <Speakers speakers={organizers} />
+          {Object.keys(organizers).map(location => {
+            return (
+              <>
+                <h2
+                  css={`
+                    margin-bottom: 20px;
+                  `}
+                >
+                  {capitalize(location)}
+                </h2>
+                <Speakers speakers={organizers[location]} />
+              </>
+            )
+          })}
         </Panel>
       </main>
     </Layout>
