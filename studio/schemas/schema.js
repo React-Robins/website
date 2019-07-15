0b225006143a7759830e46ff7ccd9728e460baf9
Sara <hey@iamsaravieira.com>
2019-07-15 20:38:15 +0200

    upgrade

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -12,23 +12,7 @@ import thanks from './documents/thanks'
 import organizer from './documents/organizer'
 import siteSettings from './documents/siteSettings'
 
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
   name: 'queerjs',
-  // Then proceed to concatenate our our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    siteSettings,
-    speaker,
-    sponsor,
-    attendee,
-    thanks,
-    organizer
-
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-  ])
+  types: schemaTypes.concat([siteSettings, speaker, sponsor, attendee, thanks, organizer])
 })
