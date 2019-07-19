bff5ee71ce6d546b7841089b778d6b65196c628f
Sara <hey@iamsaravieira.com>
2019-07-19 15:10:22 +0300

    fiz dataset

--- web/src/components/Figure.js
+++ web/src/components/Figure.js
@@ -5,7 +5,7 @@ import { getFluidGatsbyImage } from 'gatsby-source-sanity'
 export default ({ node, dataset }) => {
   const config = {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'atggkqis',
-    dataset: process.env.GATSBY_SANITY_DATASET || dataset || 'production',
+    dataset: dataset || 'production',
     token: process.env.SANITY_READ_TOKEN
   }
   const fluidProps = getFluidGatsbyImage(node.asset._id, { maxWidth: 675 }, config)
