aca7bdc9e0e2a6d9a81e19c39a80cf1a99ab0a8c
Laura buns <lawr@lauragonzalez.cc>
2019-07-19 15:00:24 +0300

    responsive

--- web/src/components/City/elements.js
+++ web/src/components/City/elements.js
@@ -5,23 +5,37 @@ export const Wrapper = styled(Link)`
   display: flex;
   align-items: center;
   text-decoration: none;
-  padding-top: 40px;
-  padding-bottom: 40px;
+  padding: 20px;
   border: 3px solid #605367;
   justify-content: center;
   margin-bottom: -3px;
+  flex-direction: column;
+  @media (min-width: 60em) {
+    flex-direction: row;
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
 `
 export const sizes = css`
-  width: 190px;
-  height: 190px;
+  width: 95px;
+  height: 95px;
+  @media (min-width: 60em) {
+    width: 190px;
+    height: 190px;
+  }
 `
 
 export const CityInfo = styled.div`
   display: flex;
   flex-direction: column;
-  margin-left: 40px;
-  flex-basis: 50%;
-  align-items: flex-start;
+  text-align: center;
+  margin-top: 30px;
+  @media (min-width: 60em) {
+    flex-basis: 50%;
+    margin-left: 40px;
+    margin-top: 0;
+    align-items: flex-start;
+  }
 `
 
 export const CityIcon = styled.div`
