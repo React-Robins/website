85de365f41b8b82c8aa968281893a483452f345c
Sara <hey@iamsaravieira.com>
2019-07-25 20:02:07 +0200

    add mc again

---
+++ web/src/pages/_main.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import SEO from '../components/seo'
+import Layout from '../containers/layout'
+import Sponsors from '../components/Sponsors'
+import Info from '../components/Info'
+import Attendees from '../components/Attendees'
+import Speakers from '../components/Speakers'
+import Thanks from '../components/Thanks'
+import Panel from '../components/Panel'
+
+const Main = ({ city, dataset }) => {
+  const { site, organizers, mainOrganizer, thanks, speakers, attendees, sponsors } = city
+
+  return (
+    <Layout>
+      <SEO title={site.title} description={site.description} />
+      <main>
+        <h1 hidden>Welcome to {site.title}</h1>
+        <Info site={site} dataset={dataset} />
+        <Panel heading="What?">
+          <p
+            css={`
+              font-family: 'NeutraText-Bold';
+              font-size: 18px;
+              line-height: 28px;
+            `}
+          >
+            This is a meetup where anyone is welcome to attend and support the speakers and the idea
+            but all the speakers will be Queer.
+            <br />
+            This meetup exists to give a voice to everyone, to make a safe space where everyone is
+            welcome.
+            <br />
+            Join us! There will be food and stickers 🌈
+          </p>
+        </Panel>
+        <Panel heading="Speakers">
+          <Speakers dataset={dataset} cfp={site.cfp} speakers={speakers.filter(s => !s.mc)} />
+        </Panel>
+        {speakers.filter(s => s.mc).length ? (
+          <Panel heading="MC">
+            <Speakers
+              noSpeak
+              dataset={dataset}
+              cfp={site.cfp}
+              speakers={speakers.filter(s => s.mc)}
+            />
+          </Panel>
+        ) : null}
+        <Panel heading={`Attendees (${attendees.length})`}>
+          <Attendees attendees={attendees} />
+        </Panel>
+
+        <Panel heading="Sponsors">
+          <Sponsors sponsors={sponsors} />
+        </Panel>
+      </main>
+      <Panel heading="Special Thanks">
+        <Thanks
+          organizers={organizers}
+          thanks={thanks}
+          site={site}
+          mainOrganizer={mainOrganizer.find(o => o.main)}
+        />
+      </Panel>
+    </Layout>
+  )
+}
+
+export default Main
--- web/src/pages/berlin.js
+++ web/src/pages/berlin.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import SEO from '../components/seo'
-import Layout from '../containers/layout'
-import Sponsors from '../components/Sponsors'
-import Info from '../components/Info'
-import Attendees from '../components/Attendees'
-import Speakers from '../components/Speakers'
-import Thanks from '../components/Thanks'
-import Panel from '../components/Panel'
+import Main from './_main'
 
 export const query = graphql`
   query {
@@ -69,62 +62,7 @@ export const query = graphql`
     }
   }
 `
-
-const IndexPage = ({ data = {} }) => {
-  const {
-    berlin: { site, organizers, mainOrganizer, thanks, speakers, attendees, sponsors }
-  } = data
-
-  return (
-    <Layout>
-      <SEO title={site.title} description={site.description} />
-      <main>
-        <h1 hidden>Welcome to {site.title}</h1>
-        <Info site={site} dataset="production" />
-        <Panel heading="What?">
-          <p
-            css={`
-              font-family: 'NeutraText-Bold';
-              font-size: 18px;
-              line-height: 28px;
-            `}
-          >
-            This is a meetup where anyone is welcome to attend and support the speakers and the idea
-            but all the speakers will be Queer.
-            <br />
-            This meetup exists to give a voice to everyone, to make a safe space where everyone is
-            welcome.
-            <br />
-            Join us! There will be food and stickers 🌈
-            <br />
-            <br />
-            <b>
-              All the talks will be livestream, if you don't wish to be in any of the videos please
-              let us any of the organizers know
-            </b>
-          </p>
-        </Panel>
-        <Panel heading="Speakers">
-          <Speakers cfp={site.cfp} speakers={speakers} />
-        </Panel>
-        <Panel heading={`Attendees (${attendees.length})`}>
-          <Attendees attendees={attendees} />
-        </Panel>
-
-        <Panel heading="Sponsors">
-          <Sponsors sponsors={sponsors} />
-        </Panel>
-      </main>
-      <Panel heading="Special Thanks">
-        <Thanks
-          organizers={organizers}
-          thanks={thanks}
-          site={site}
-          mainOrganizer={mainOrganizer.find(o => o.main)}
-        />
-      </Panel>
-    </Layout>
-  )
+export default ({ data = {} }) => {
+  const { berlin } = data
+  return <Main city={berlin} dataset="production" />
 }
-
-export default IndexPage
