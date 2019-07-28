f01ab3754ee602c878b579ada73d66a9deb0bfcb
Ania <ania.makes@gmail.com>
2019-07-28 12:06:11 +0100

    Added dedupe function and remove @ from the ghLink

--- web/src/components/Attendees/index.js
+++ web/src/components/Attendees/index.js
@@ -3,12 +3,37 @@ import React from 'react'
 import { Attendees } from './elements'
 import shuffle from '../../helpers/shuffle'
 
+const dedupeAttendees = (attendeesArray) => {
+  const list = [];
+  const deduped = ourList.reduce((acc, current) => {
+    const cleanGhLink = current.ghLink.startsWith('@') ? current.ghLink.slice(1) : current.ghLink
+
+
+    if (list.includes(cleanGhLink)) {
+      return acc;
+    }
+
+    list.push(cleanGhLink)
+    acc.push(current)
+
+    return acc
+  }, [])
+
+  return deduped
+}
+
 export default ({ attendees }) => {
+
+  const dedupedAttendees = dedupeAttendees(attendees)
+
   return (
     <Attendees>
-      {shuffle(attendees).map(a => {
-       // Pretty ugly but this way we do not get duplicate https://github.com urls
-       const ghLink = `https://github.com/${a.ghLink.trim().replace('https://github.com/', '')}`;
+      {shuffle(dedupedAttendees).map(a => {
+        // Pretty ugly but this way we do not get duplicate https://github.com urls
+
+        const cleanGhLink = a.ghLink.startsWith('@') ? a.ghLink.slice(1) : a.ghLink
+
+        const ghLink = `https://github.com/${cleanGhLink.trim().replace('https://github.com/', '')}`;
         return (<li key={a.id}>
           <a
             href={ghLink}
