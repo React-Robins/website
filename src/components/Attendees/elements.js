8ba491a16add3a5f2ef67f779424e0d4767c3ed7
Sara <hey@iamsaravieira.com>
2019-09-04 18:45:43 +0200

    fix broken images

--- src/components/Attendees/elements.js
+++ src/components/Attendees/elements.js
@@ -10,5 +10,21 @@ export const Attendees = styled.ul`
     border-radius: 50%;
     margin-left: -4px;
     margin-top: -4px;
+    text-indent: -9999px;
+    height: 46px;
+    width: 46px;
+    height: auto;
+    display: block;
+    position: relative;
+    :before {
+      content: ' ';
+      display: block;
+      position: absolute;
+      border-radius: 50%;
+      left: 0;
+      height: 46px;
+      width: 100%;
+      background-color: rgb(230, 230, 230);
+    }
   }
 `
