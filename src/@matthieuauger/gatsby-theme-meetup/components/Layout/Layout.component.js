9bd9620dc7f1df159938b99aba7c54bb4e0a749c
Silvia Rebelo <silvi04@gmail.com>
2019-10-01 19:26:27 +0100

    Adding smaller version of favicon that works

--- src/@matthieuauger/gatsby-theme-meetup/components/Layout/Layout.component.js
+++ src/@matthieuauger/gatsby-theme-meetup/components/Layout/Layout.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
-import favicon from '../../images/favicon.png';
+import favicon from '../../images/favicon-16x16.png';
 
 import Header from '../Header';
 import './layout.css';
